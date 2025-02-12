ac6b20f6d9d484ae58de9ba702b585e9644ff4c5
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-11 21:48:19 -0500

    Completed lesson 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (startInt, endInt) {
+  let allNums = [];
+  let sum = 0;
 
+  if (
+    startInt < 0 ||
+    endInt < 0 ||
+    !Number.isInteger(startInt) ||
+    !Number.isInteger(endInt)
+  ) {
+    return "ERROR";
+  }
+
+  // Logic
+
+  if (startInt > endInt) {
+    for (let i = endInt; i <= startInt; i++) {
+      allNums.push(i);
+    }
+  } else {
+    for (let i = startInt; i <= endInt; i++) {
+      allNums.push(i);
+    }
+  }
+
+  for (const i in allNums) {
+    sum += allNums[i];
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
