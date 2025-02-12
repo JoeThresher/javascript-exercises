0b91333151edd30f37ef1f3865dc250fa55e664a
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-11 20:54:04 -0500

    Completed lesson 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (passedArray, ...args) {
+  let returnArray = [];
+
+  for (const i in passedArray) {
+    for (const j in args) {
+      if (passedArray[i] === args[j]) {
+        delete passedArray[i];
+      }
+    }
+  }
+
+  // filters out non-truthy array elements, since delete leaves undefined variables in the array
+  return passedArray.filter(Boolean);
 };
 
 // Do not edit below this line
