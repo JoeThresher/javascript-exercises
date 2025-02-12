17bdcfe35823f2fa30e039125d4075ff0cd41ff9
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-11 19:17:42 -0500

    Completed lesson 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (repeatWord, numRepeats) {
+  let repeated = "";
 
+  if (numRepeats < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < numRepeats; i++) {
+    repeated = repeated + repeatWord;
+  }
+  return repeated;
 };
 
 // Do not edit below this line
