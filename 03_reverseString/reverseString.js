e939542957748feaaaf0430ac17158a97410fde1
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-11 19:44:56 -0500

    Completed lesson 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (forwardString) {
+  let stringArray = forwardString.split("");
+  let reverseStringArray = [];
 
+  for (const i in stringArray) {
+    reverseStringArray.unshift(stringArray[i]);
+  }
+
+  return reverseStringArray.join("");
 };
 
 // Do not edit below this line
