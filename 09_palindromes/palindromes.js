f4db7125c1a8b959fef6e8a81e96175e38785a41
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-14 16:35:39 -0500

    Completed lesson 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (contestant) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz123456789";
 
+  const stringClean = contestant
+    .toLowerCase()
+    .split("")
+    .filter((char) => validCharacters.includes(char))
+    .join("");
+
+  let reverse = stringClean.split("").reverse().join("");
+
+  if (stringClean === reverse) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
