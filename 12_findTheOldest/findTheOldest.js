9b2a811d9956d113734fbc6bb0a97870b2327075
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-15 16:32:08 -0500

    Started lesson 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let ageArr = people.map((person) => {
+    if (person.includes(yearOfBirth) && person.includes(yearOfDeath)) {
+      return person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+  return ageArr;
 };
 
 // Do not edit below this line
