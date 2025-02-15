d7ddf6975af280c29ffe9531e146678a1b9bc077
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-02-15 16:06:51 -0500

    Completed lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+  // let fibArr = [1, 1];
+  // let sumArr = [];
+  // let curSum;
 
+  // switch (x) {
+  //   case 0:
+  //     return 0;
+  //   case 1:
+  //     return 1;
+  // }
+
+  // for (let i = 0; i < 200; i++) {
+  //   sumArr = fibArr.slice(-1);
+  //   curSum = sumArr[0] + sumArr[1];
+  //   fibArr = fibArr.concat(curSum);
+  // }
+
+  // return fibArr[x];
+
+  if (x < 0) {
+    return "OOPS";
+  }
+
+  let fib = [0, 1];
+
+  for (let i = 2; i < 200; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+
+  x = Number(x);
+  return fib[x];
 };
 
 // Do not edit below this line
